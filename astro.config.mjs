a326a41af5f365c5059cee13b9ea2bd46440c47a
Chetan Verma <chetan61verma@gmail.com>
2025-03-15 17:07:06 +0530

    Configure for Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import vercel from '@astrojs/vercel/serverless';
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), mdx()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare({
+    mode: "directory",
+  }),
 });
